3027eecef88767565f5983cee08f7d0daf264fbb
Dinesh <ranad4508@gmail.com>
2025-02-26 09:01:52 +0545

    Commit message for the merge

--- positiveNumber.js
+++ positiveNumber.js
@@ -1,3 +1,10 @@
 const number = [1, 5, -3, 10, -5];
+const negativeNumber = number.filter((num) => num < 0);
+console.log("Negative numbers:", negativeNumber);
 const positiveNumber = number.filter((num) => num > 0);
 console.log("Positive numbers:", positiveNumber);
+
+const evenNumber = number.filter((num) => num % 2 === 0);
+console.log("Even numbers:", evenNumber);
+const oddNumber = number.filter((num) => num % 2 !== 0);
+console.log("Odd numbers:", oddNumber);
